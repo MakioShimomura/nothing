34e333c3006ddf44a2ec7aa55accce8f84d49725
ShimomuraMakio <shimomura.makio@gmail.com>
2024-03-09 13:28:42 +0900

    電卓クリック時の挙動修正

---
+++ ui.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  const display = document.querySelector('.js-display');
+  display.classList.add('active');
+  setTimeout(function () {
+    display.classList.remove('active');
+  }, 1000);
+
+  const buttons = document.querySelectorAll('.js-button');
+  buttons.forEach(function (button) {
+    button.classList.add('active');
+    setTimeout(function () {
+      button.classList.remove('active');
+    }, 1000);
+
+    button.addEventListener('click', () => {
+      display.classList.add('active');
+      setTimeout(function () {
+        display.classList.remove('active');
+      }, 800);
+    });
+  });
+};
