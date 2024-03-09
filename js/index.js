daeb2648f5fd7fd63060a7177260f7750646fc20
kuma <kuma@sonicgarden.jp>
2024-03-09 18:06:47 +0900

    バグ修正

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ const OPERATORS = ['+', '-', '×', '÷']
 const PERFECT_NUMBERS = [6, 28, 496, 8128, 33550336, 8589869056, 137438691328, 2305843008139952128]
 
 const displayResult = () => {
-  let result = eval(document.calculator__form.display.value)
   if (document.calculator__form.display.value == '') {
     document.calculator__form.display.value = 0
-  } else if (PERFECT_NUMBERS.includes(result)) {
-    appearCalculator()
-    document.calculator__form.display.value = result
   } else if (!OPERATORS.includes(document.calculator__form.display.value.slice(-1))) {
     document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
     document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
+    let result = eval(document.calculator__form.display.value)
     document.calculator__form.display.value = result
+    if (PERFECT_NUMBERS.includes(result)) {
+      appearCalculator()
+    }
   }
 }
 
