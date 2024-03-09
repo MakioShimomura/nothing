c1c37d92500802550a718a1748b80290b0c22f5a
kuma <kuma@sonicgarden.jp>
2024-03-09 15:30:20 +0900

    何も入力していない時は０が入るように

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ function get_calc(btn) {
   const operatorValue = ['+', '-', '×', '÷']
   const displayValueLastChar = document.calculator__form.display.value.slice(-1)
   if (btn.value == "=") {
-    if (!operatorValue.includes(displayValueLastChar) && displayValueLastChar == '=') {
+    if (document.calculator__form.display.value == '') {
+      document.calculator__form.display.value = 0
+    } else if (!operatorValue.includes(displayValueLastChar)) {
       document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
       document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
       document.calculator__form.display.value = eval(document.calculator__form.display.value);
     }
   } else if (btn.value == "C") {
-    document.calculator__form.display.value = "";
+    document.calculator__form.display.value = '';
   } else {
     if (operatorValue.includes(displayValueLastChar) && operatorValue.includes(btn.value)) {
       document.calculator__form.display.value = document.calculator__form.display.value.replace(/.$/, btn.value)
     } else {
+      if (document.calculator__form.display.value.slice(0, 1) == '0') {
+        document.calculator__form.display.value = document.calculator__form.display.value.replace(/^./, '')
+      }
       document.calculator__form.display.value += btn.value;
     }
   }
