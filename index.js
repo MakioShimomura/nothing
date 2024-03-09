e157a1f45a26b4905e32c8ee1fc21abbe1d9e96a
kuma <kuma@sonicgarden.jp>
2024-03-09 14:36:09 +0900

    電卓の機能改修

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function get_calc(btn) {
+  const operatorValue = ['+', '-', '×', '÷']
+  const displayValueLastChar = document.calculator__form.display.value.slice(-1)
   if (btn.value == "=") {
-    document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
-    document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
-    document.calculator__form.display.value = eval(document.calculator__form.display.value);
+    if (!operatorValue.includes(displayValueLastChar)) {
+      document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
+      document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
+      document.calculator__form.display.value = eval(document.calculator__form.display.value);
+    }
   } else if (btn.value == "C") {
     document.calculator__form.display.value = "";
   } else {
-    document.calculator__form.display.value += btn.value;
+    if (operatorValue.includes(displayValueLastChar) && operatorValue.includes(btn.value)) {
+      document.calculator__form.display.value = document.calculator__form.display.value.replace(/.$/, btn.value)
+    } else {
+      document.calculator__form.display.value += btn.value;
+    }
   }
 }
