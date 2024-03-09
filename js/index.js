fde3bfaa3034f65cdd0a3f4dfd5e4bcb0403e98d
kuma <kuma@sonicgarden.jp>
2024-03-09 16:59:16 +0900

    リファクタ

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
-function get_calc(btn) {
-  const operatorValue = ['+', '-', '×', '÷']
+const OPERATORS = ['+', '-', '×', '÷']
+
+function getCalc(btn) {
   const displayValueLastChar = document.calculator__form.display.value.slice(-1)
   if (btn.value == "=") {
-    if (document.calculator__form.display.value == '') {
-      document.calculator__form.display.value = 0
-    } else if (!operatorValue.includes(displayValueLastChar)) {
-      document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
-      document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
-      document.calculator__form.display.value = eval(document.calculator__form.display.value);
-    }
+    displayResult(displayValueLastChar)
   } else if (btn.value == "C") {
     document.calculator__form.display.value = '';
   } else {
-    if (operatorValue.includes(displayValueLastChar) && operatorValue.includes(btn.value)) {
-      document.calculator__form.display.value = document.calculator__form.display.value.replace(/.$/, btn.value)
-    } else {
-      if (document.calculator__form.display.value.slice(0, 1) == '0') {
-        document.calculator__form.display.value = document.calculator__form.display.value.replace(/^./, '')
-      }
-      document.calculator__form.display.value += btn.value;
+    controlDisplay(displayValueLastChar, btn)
+  }
+}
+
+const displayResult = (displayValueLastChar) => {
+  if (document.calculator__form.display.value == '') {
+    document.calculator__form.display.value = 0
+  } else if (!OPERATORS.includes(displayValueLastChar)) {
+    document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
+    document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
+    document.calculator__form.display.value = eval(document.calculator__form.display.value);
+  }
+}
+
+const controlDisplay = (displayValueLastChar, btn) => {
+  if (OPERATORS.includes(displayValueLastChar) && OPERATORS.includes(btn.value)) {
+    document.calculator__form.display.value = document.calculator__form.display.value.replace(/.$/, btn.value)
+  } else {
+    if (document.calculator__form.display.value.slice(0, 1) == '0') {
+      document.calculator__form.display.value = document.calculator__form.display.value.replace(/^./, '')
     }
+    document.calculator__form.display.value += btn.value;
   }
 }
