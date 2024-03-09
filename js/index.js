6fe75ff19ea95d3428931da5a1feb5e5e14dc7f8
kuma <kuma@sonicgarden.jp>
2024-03-09 17:23:47 +0900

    リファクタ

--- js/index.js
+++ js/index.js
@@ -1,28 +1,17 @@
 const OPERATORS = ['+', '-', '×', '÷']
 
-function getCalc(btn) {
-  const displayValueLastChar = document.calculator__form.display.value.slice(-1)
-  if (btn.value == "=") {
-    displayResult(displayValueLastChar)
-  } else if (btn.value == "C") {
-    document.calculator__form.display.value = '';
-  } else {
-    controlDisplay(displayValueLastChar, btn)
-  }
-}
-
-const displayResult = (displayValueLastChar) => {
+const displayResult = () => {
   if (document.calculator__form.display.value == '') {
     document.calculator__form.display.value = 0
-  } else if (!OPERATORS.includes(displayValueLastChar)) {
+  } else if (!OPERATORS.includes(document.calculator__form.display.value.slice(-1))) {
     document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
     document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
     document.calculator__form.display.value = eval(document.calculator__form.display.value);
   }
 }
 
-const controlDisplay = (displayValueLastChar, btn) => {
-  if (OPERATORS.includes(displayValueLastChar) && OPERATORS.includes(btn.value)) {
+const controlDisplay = (btn) => {
+  if (OPERATORS.includes(document.calculator__form.display.value.slice(-1)) && OPERATORS.includes(btn.value)) {
     document.calculator__form.display.value = document.calculator__form.display.value.replace(/.$/, btn.value)
   } else {
     if (document.calculator__form.display.value.slice(0, 1) == '0') {
@@ -30,4 +19,8 @@ const controlDisplay = (displayValueLastChar, btn) => {
     }
     document.calculator__form.display.value += btn.value;
   }
+}
+
+const clearDisplay = () => {
+  document.calculator__form.display.value = '';
 }
