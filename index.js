b357e83f2fb94c20d32420dc50bcf965d700746b
kuma <kuma@sonicgarden.jp>
2024-03-09 14:38:33 +0900

    =２回押してもundefinedにならないように

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function get_calc(btn) {
   const operatorValue = ['+', '-', '×', '÷']
   const displayValueLastChar = document.calculator__form.display.value.slice(-1)
   if (btn.value == "=") {
-    if (!operatorValue.includes(displayValueLastChar)) {
+    if (!operatorValue.includes(displayValueLastChar) && displayValueLastChar == '=') {
       document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('×', '*')
       document.calculator__form.display.value = document.calculator__form.display.value.replaceAll('÷', '/')
       document.calculator__form.display.value = eval(document.calculator__form.display.value);
